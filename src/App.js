a6a786dc61ea5245cf5d450cc980d16d607be290
lakshya_kanungo <lakshya.ecowrap@gmail.com>
2023-02-02 16:24:10 +0530

    useEffect hook explained

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [Counter2, setCounter2] = useState(0);
+
+  // useEffect(() => {
+  //   first
+
+  //   // return () => {
+  //   //   second
+  //   // }
+  // }, [a,b,c,d])
+
+  useEffect(() => {
+    document.title = `You clicked ${counter} times but Counter2 is :${Counter2}`;
+  }, [Counter2]);
+
+  function handleClick() {
+    setCounter(counter + 1);
+    if (counter % 2 == 0) setCounter2(counter);
+  }
+
+  /*
+  useEffect will run some function only after our dom has been rendered
+
+  if we dont have dependency array then useEffect will run on first render and also on every re-render
+  if we have an empty dependency array then the useEffect will run only on first render
+
+  */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Your counter value is : {counter}</p>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Counter
+      </button>
     </div>
   );
 }
